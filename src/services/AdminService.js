0b584068ce8594299d30bfd09572a158f3b477af
vinoth2802 <vinothkumarramyajeeva@gmail.com>
2025-02-16 19:58:53 +0530

    initial commit

--- src/services/AdminService.js
+++ src/services/AdminService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const API_URL = `${import.meta.env.VITE_API_URL}/admin`;
+const API_URL = `${import.meta.env.VITE_API_URL}/admin/`;
 
 export const getAdmin =(username) => axios.get(API_URL + username);
